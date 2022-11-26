c24c23b663eb8a3278972cd0c6a1681095a15243
Felipe Ferreira <fbhz85@gmail.com>
2022-11-26 09:53:22 -0300

    spinner

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -3,16 +3,19 @@ import { useContext } from 'react'
 
 import FeedbackItem from "./FeedbackItem"
 import FeedbackContext from '../context/FeedbackContext'
+import Spinner from './shared/Spinner'
 
 function FeedbackList() {
 
-const {feedback} = useContext(FeedbackContext)
+const {feedback, isLoading} = useContext(FeedbackContext)
 
-  if (!feedback || feedback.length === 0) {
+  if (!isLoading && (!feedback || feedback.length === 0)) {
     return <p>No Feedback Yet</p>
   }
 
-  return <div className="feedback-list">
+  return isLoading ? (<Spinner />) : (
+
+  <div className="feedback-list">
     <AnimatePresence >
   {feedback.map((item) => (
     <motion.div
@@ -27,7 +30,7 @@ const {feedback} = useContext(FeedbackContext)
   ))}
   </AnimatePresence>
 </div>
-
+  )
   // return <div className="feedback-list">
   //   {feedback.map((item) => (
   //     <FeedbackItem 
--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,32 +1,28 @@
 import { v4 as uuidv4 } from 'uuid'
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({ children }) => {
-    const [feedback, setFeedback] = useState([
-        {
-            id: 1,
-            rating: 10,
-            text: 'ITEM 01 - Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
-          },
-          {
-            id: 2,
-            rating: 9,
-            text: 'ITEM 02 - Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
-          },
-          {
-            id: 3,
-            rating: 8,
-            text: 'ITEM 03 - Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
-          },
-    ])
-
+    const [isLoading, setIsLoading] = useState(true)
+    const [feedback, setFeedback] = useState([])
     const [feedbackEdit, setFeedbackEdit] = useState({
         item: {},
         edit: false
     })
+
+    useEffect(() => {
+        fetchFeedback()
+    }, [])
+
+    //fetch feedback
+    const fetchFeedback = async () => {
+        const response = await fetch(`http://localhost:5000/feedback?sort=id&_order=desc`)
+        const data = await response.json()
+        setFeedback(data)
+        setIsLoading(false)
+    }
 
     //add feedback
     const addFeedback = (newFeedback) => {
@@ -58,7 +54,7 @@ export const FeedbackProvider = ({ children }) => {
     return (
         <FeedbackContext.Provider
         value={{
-            feedback, feedbackEdit, deleteFeedback, addFeedback, editFeedback, updateFeedback
+            feedback, feedbackEdit, isLoading, deleteFeedback, addFeedback, editFeedback, updateFeedback
         }}>
             {children}
         </FeedbackContext.Provider>
